28c5da5e11f72691bab340359878ab37f2e15684
Jhnsk <150840061+Jhnsk@users.noreply.github.com>
2024-06-21 08:41:24 -0300

    Add files via upload

---
+++ torreJson.js
@@ -0,0 +1,19 @@
+let torreJson = [
+  {nome:'TORRE',
+  span:'AMSTEL 1L',
+   preço: 15.90,
+   sub: 'TORRE',
+   subs:'AMSTEL 2L',
+   p: 28.90,
+   img: '/images/torre.jpeg'
+  },
+  
+     {nome:'TORRE',
+  span:'DEVASSA 1L',
+   preço: 13.90,
+   sub: 'TORRE',
+   subs:'DEVASSA 2L',
+   p: 26.90,
+   img: '/images/torre.jpeg'
+  }
+  ]
